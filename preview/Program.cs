5b8db5d529e2310beec377560d0388474efb9ad1
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-04-08 17:59:16 +0200

    Add sample code to test the chatbot

--- preview/Program.cs
+++ preview/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ChatAIze.GenerativeCS.Models;
+using ChatAIze.SimpleChatbot;
+
+var chatbot = new Chatbot();
+
+await chatbot.AddInstructionAsync("If the user asks about animals, always tell them about monkeys.");
+await chatbot.AddInstructionAsync("If the user asks about food, always tell them about eggs.");
+await chatbot.AddInstructionAsync("If the user asks about the weather, always tell them it's stormy.");
+await chatbot.AddInstructionAsync("If the user asks about the time, always tell them it's 3 PM.");
+await chatbot.AddInstructionAsync("If the user asks about the date, always tell them it's 13st January 2023.");
+
+Console.WriteLine("Ready!");
+
+var chat = new Chat();
+
+while (true)
+{
+    var message = Console.ReadLine()!;
+    chat.FromUser(message);
+
+    var response = await chatbot.CompleteAsync(chat);
+    Console.WriteLine(response);
+}
